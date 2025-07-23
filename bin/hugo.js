ba607304f8819241350d8e442c438970325d838f
Ozgur Vurgun <ozgurvurgun35@gmail.com>
2025-07-24 00:40:40 +0300

    first commit

---
+++ bin/hugo.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+import { execSync } from "child_process";
+import path from "path";
+import Encryptor from "../lib/Encryptor.js";
+
+const ROOT_DIR = process.cwd();
+const CONTENT_DIR = path.join(ROOT_DIR, "content");
+
+async function main() {
+  const encryptor = new Encryptor(CONTENT_DIR);
+  await encryptor.processAll();
+
+  execSync("hugo", { stdio: "inherit" });
+}
+
+main();
